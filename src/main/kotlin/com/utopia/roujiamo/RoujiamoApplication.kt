3c45dbbb5416221497d0d46c8039b1af011cacda
yupenglei <yupenglei@kuaishou.com>
2024-09-14 16:06:13 +0800

    init

---
+++ src/main/kotlin/com/utopia/roujiamo/RoujiamoApplication.kt
@@ -0,0 +1,11 @@
+package com.utopia.roujiamo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoujiamoApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoujiamoApplication>(*args)
+}
---
+++ src/test/kotlin/com/utopia/roujiamo/RoujiamoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.utopia.roujiamo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoujiamoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
