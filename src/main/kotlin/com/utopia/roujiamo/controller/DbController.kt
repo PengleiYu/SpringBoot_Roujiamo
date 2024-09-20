e061c27845b8e9f856eaf49d701c2ada00139b47
yupenglei <yupenglei@kuaishou.com>
2024-09-20 18:41:01 +0800

    模板正确展示数据

--- src/main/kotlin/com/utopia/roujiamo/controller/DbController.kt
+++ src/main/kotlin/com/utopia/roujiamo/controller/DbController.kt
@@ -1,5 +1,7 @@
 package com.utopia.roujiamo.controller
 
+import com.utopia.roujiamo.bean.Ingredient
+import com.utopia.roujiamo.repository.IngredientRepository
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -9,7 +11,10 @@ import javax.sql.DataSource
 
 @Controller
 @RequestMapping("/database")
-class DbController(private val dataSource: DataSource) {
+class DbController(
+    private val dataSource: DataSource,
+    private val ingredientRepo: IngredientRepository
+) {
     @GetMapping("/url")
     @ResponseBody
     fun getDbUrl(): String = dataSource.connection.metaData.url
@@ -19,8 +24,11 @@ class DbController(private val dataSource: DataSource) {
         return "ingredient"
     }
 
-    @ModelAttribute("ingredient")
-    fun getIngredientMode(): String {
-        return "配料模型"
+    @ModelAttribute("ingredientMap")
+    fun getIngredientMode(): Map<String, List<Ingredient>> {
+        return ingredientRepo.findAll()
+            .groupBy(Ingredient::type)
+            .toList()
+            .associate { (it.first?.name?.uppercase() ?: "") to it.second }
     }
 }
